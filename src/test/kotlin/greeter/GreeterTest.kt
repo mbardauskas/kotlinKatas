dca1be80d7e3eb9962f89f31b26e9096b5b14fda
Martynas Bardauskas <martynasb@wix.com>
2019-02-07 15:23:17 +0200

    import from junit4 instead of junit5

--- src/test/kotlin/greeter/GreeterTest.kt
+++ src/test/kotlin/greeter/GreeterTest.kt
@@ -1,53 +1,53 @@
 package greeter
 
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 
 
 class GreeterTest {
     private lateinit var uut: Greeter
     private val regularTimeGetter: () -> Int = { 14 }
 
-    @BeforeEach
+    @Before
     fun beforeEach() {
         uut = Greeter(regularTimeGetter)
     }
 
     @Test
     fun ReturnsHelloName() {
-        Assertions.assertEquals("Hello Bob", uut.greet("Bob"))
+        Assert.assertEquals("Hello Bob", uut.greet("Bob"))
     }
 
     @Test
     fun TrimsTheInput() {
-        Assertions.assertEquals("Hello Bob", uut.greet("   Bob    "))
+        Assert.assertEquals("Hello Bob", uut.greet("   Bob    "))
     }
 
     @Test
     fun CapitalizesFirstLetter() {
-        Assertions.assertEquals("Hello Tom", uut.greet("tom"))
+        Assert.assertEquals("Hello Tom", uut.greet("tom"))
     }
 
     @Test
     fun ReturnsGoodMorningWhenTimeIsBetween6and12() {
         val morningTimeGetter: () -> Int = { 6 }
         uut = Greeter(morningTimeGetter)
-        Assertions.assertEquals("Good morning Tom", uut.greet("tom"))
+        Assert.assertEquals("Good morning Tom", uut.greet("tom"))
     }
 
     @Test
     fun ReturnsGoodEveningWhenTimeIsBetween18and22() {
         val eveningTimeGetter: () -> Int = { 18 }
         uut = Greeter(eveningTimeGetter)
-        Assertions.assertEquals("Good evening Tom", uut.greet("tom"))
+        Assert.assertEquals("Good evening Tom", uut.greet("tom"))
     }
 
     @Test
     fun ReturnsGoodNightWhenTimeIsBetween22and6() {
         val nightTimeGetter: () -> Int = { 22 }
         uut = Greeter(nightTimeGetter)
-        Assertions.assertEquals("Good night Tom", uut.greet("tom"))
+        Assert.assertEquals("Good night Tom", uut.greet("tom"))
     }
 
     @Test
--- src/test/kotlin/stringCalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringCalculator/StringCalculatorTest.kt
@@ -1,45 +1,46 @@
 package stringCalculator
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import java.lang.Exception
+//import java.lang.Exception
+import org.junit.Assert
+import org.junit.Test
 
 class CalculatorTest {
     private val stringCalc = StringCalculator()
 
     @Test
     fun EmptyStringReturnsNull() {
-        Assertions.assertEquals(null, stringCalc.add(""))
+        Assert.assertEquals(null, stringCalc.add(""))
     }
 
     @Test
     fun SimpleStringReturnsInteger() {
-        Assertions.assertEquals(3, stringCalc.add("3"))
+        Assert.assertEquals(3, stringCalc.add("3"))
     }
 
     @Test
     fun CommaDelimitedNumbersReturnSum() {
-        Assertions.assertEquals(10, stringCalc.add("3,7"))
+        Assert.assertEquals(10, stringCalc.add("3,7"))
     }
 
     @Test
     fun NewLineDelimitedNumbersReturnSum() {
-        Assertions.assertEquals(15, stringCalc.add("5\n5\n5"))
+        Assert.assertEquals(15, stringCalc.add("5\n5\n5"))
     }
 
     @Test
     fun BothDelimitationsReturnSumFromOneString() {
-        Assertions.assertEquals(15, stringCalc.add("5\n5,5"))
+        Assert.assertEquals(15, stringCalc.add("5\n5,5"))
     }
 
-    @Test
-    fun NegativeNumbersThrowException() {
-        val exception: Exception = Assertions.assertThrows(IllegalArgumentException::class.java) { stringCalc.add("-1,5,-3") }
-        Assertions.assertEquals("Negatives not allowed: -1, -3", exception.message)
-    }
+//    @Test(expected = javaClass<IllegalArgumentException>)
+//    fun NegativeNumbersThrowException() {
+//        javaClass<IllegalArgumentException>
+//        val exception: Exception = Assert.assert(IllegalArgumentException::class.java) { stringCalc.add("-1,5,-3") }
+//        Assert.assertEquals("Negatives not allowed: -1, -3", exception.message)
+//    }
 
     @Test
     fun NumbersGreaterThan1000AreIgnored() {
-        Assertions.assertEquals(15, stringCalc.add("5\n5,5,1001"))
+        Assert.assertEquals(15, stringCalc.add("5\n5,5,1001"))
     }
 }
